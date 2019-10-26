8cfadf41ba66ffa25fddb5ff44f2ed9b48074a58
kubajj <vilouskubajj@gmail.com>
2019-10-26 22:28:11 +0100

    Hey Allan, I'm sorry, but I needed to finish it.

--- dressCodeApp/navigation/NavigationMenu.js
+++ dressCodeApp/navigation/NavigationMenu.js
@@ -17,21 +17,41 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 export default function NavigationMenu() {
   return (
     <View>
-	    <Header
-	    		leftComponent={{ icon: 'menu', color: '#fff' }}
-				centerComponent={{ text: 'MY TITLE', style: { color: '#fff' } }}
-				rightComponent={{ icon: 'home', color: '#fff' }}>		
-			<Image
+	    <Header 
+			containerStyle={{
+			    backgroundColor: '#A7358C',
+			    justifyContent: 'space-around',
+			}}
+			leftComponent={<Image
 	            source={
-	            	require('../assets/images/logos/dressCODE_Final.png')
+	            	require('../assets/images/logos/dressCODE_Final_inverted.png')
 	            }
-	            style={{ width: 100,
-				    height: 80,
+	            style={{ width: 200,
+				    height: 160,
 				    resizeMode: 'contain',
-				    marginTop: 3,
-				    marginLeft: -10, }}
-	          />
+				    marginLeft: 100, }}
+	          />}
+	    	rightComponent={<Image
+	            source={
+	            	require('../assets/images/logos/jp-morgan.png')
+	            }
+	            style={{ width: 80,
+				    height: 100,
+				    paddingBottom: 0,
+				    resizeMode: 'contain',
+				    alignSelf: 'flex-end'
+					}}
+	          />}>
         </Header>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+	getStartedText: {
+    fontSize: 17,
+    color: 'rgba(96,100,109, 1)',
+    lineHeight: 24,
+    textAlign: 'center',
+  },
+})
