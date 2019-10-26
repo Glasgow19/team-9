64a360ade1e32448406cc044d7490ca05e4bc700
kubajj <vilouskubajj@gmail.com>
2019-10-26 18:10:44 +0100

    dressCodeApp - Initial commit of frontend

---
+++ dressCodeApp/App.vue
@@ -0,0 +1,50 @@
+<template>
+  	<view class="container">
+
+	    <text-input
+	      class="text-input-container"
+	      placeholder="Type here to translate!"
+	      v-model="textContent"
+	    />
+	    <text class="text-container">{{textContent}}</text>
+	</view>
+</template>
+
+<script>
+import React from 'react';
+import {Text} from 'react-native';
+    export default {
+    	data: function() {
+            return {
+                textContent: ''
+            }
+        },
+        methods: {
+            renderList: function(item) {
+                return (<Text>{item.item.key}</Text>)
+            }
+        }
+    }
+</script>
+
+<style>
+body {
+	background-color: black;
+}
+.container {
+	margin-top: 50px;
+  background-color: white;
+  flex: 1;
+}
+.text-container {
+  color: blue;
+  padding: 2;
+  font-size: 22;
+}
+.text-input-container {
+  width: 300;
+  height: 40;
+  font-size: 22;
+  border-color: gray;
+}
+</style>
