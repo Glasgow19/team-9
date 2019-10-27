15960647f35bed217118564447b86266c760d339
Jan Kočka <kockahonza@gmail.com>
2019-10-27 10:39:00 +0000

    wohooo

--- dressCodeApp/screens/SavedScreen.js
+++ dressCodeApp/screens/SavedScreen.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import Menu from '../navigation/NavigationMenu';
 
-import { View, StyleSheet, Dimensions, ScrollView, Image } from 'react-native'
-import { Card, Text } from 'react-native-elements'
+import { Text, View, StyleSheet, Dimensions, ScrollView, Image } from 'react-native'
+import { Card } from 'react-native-elements'
 
 import 'whatwg-fetch'
 
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
 class ArticlePreview extends Component {
     constructor(props) {
         super(props);
-        this.state = { title: "Hello", title_picture_url: "", text: "Ahoj" };
+        this.state = { title: "", title_picture_url: "", text: "" };
         fetch('http://192.168.43.104:8000/articles/' + props["id"])
           .then(response => response.json())
           .then(responseJson => this.setState({
@@ -54,11 +54,66 @@ class ArticlePreview extends Component {
 }
 
 
+class EventPreview extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { title: "Hello", title_picture_url: "", text: "Ahoj" };
+        fetch('http://192.168.43.104:8000/events/' + props["id"])
+          .then(response => response.json())
+          .then(responseJson => this.setState({
+              title: responseJson["title"],
+              description: responseJson["description"],
+              date_time: responseJson["date_time"]
+          }));
+    }
+
+    render() {
+        return(
+            <Card
+            title={<Text h4 style={{fontSize: 42, textAlign: 'center'}}>{this.state.title}</Text>}
+            image={<Image
+                source={{ uri: this.state.title_picture_url }}
+                style={{ width: Dimensions.get('window').width , height: 200}}/>}>
+            <Text style={{marginBottom: 10}}>
+            { this.state.description }
+            </Text>
+            </Card>  
+        )
+    }
+}
+
+
+class ContentPreview extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { content_type: 'U' }
+        fetch('http://192.168.43.104:8000/contents/' + props['id'])
+          .then(response => response.json())
+          .then(responseJson => this.setState({ content_type: responseJson['content_type'] }));
+    }
+
+    render() {
+        if (this.state.content_type == "A") {
+            return(<ArticlePreview id={this.props['id']}/>);
+        } else if (this.state.content_type == "E") {
+            return(<EventPreview id={this.props['id']}/>);
+        }
+        return <Text/>
+    }
+}
+
+
 export default class SavedScreen extends Component {
     constructor(props) {
         super(props);
 
-        this.state = { contents: [4, 5] }
+        username = "kockahonza"
+
+        this.state = { contents: [2, 3] }
+        fetch('http://192.168.43.104:8000/actualusers/kockahonza')
+          .then(response => response.json())
+          .then(responseJson => this.setState({ contents: responseJson['favourites'] }));
     }
 
     render() {
@@ -66,8 +121,10 @@ export default class SavedScreen extends Component {
             <View>
             <Menu/>
             <ScrollView style={styles.scrollView}>
-            { this.state.contents.map((id_) => <ArticlePreview id={id_}/>) }
-            </ScrollView>		
+            { this.state.contents.map(function (content, i) {
+                return <ContentPreview id={content}/>
+            })}
+            </ScrollView>
             </View>
         );
     }
