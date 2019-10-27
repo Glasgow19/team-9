3e6f98714081ca28448fe13b09619603568e41eb
Jan Kočka <kockahonza@gmail.com>
2019-10-27 03:42:27 +0000

    Fixed the picture URL serialization

--- Backend/DjangoBackend/Content/serializers.py
+++ Backend/DjangoBackend/Content/serializers.py
@@ -9,9 +9,15 @@ class ContentSerializer(serializers.ModelSerializer):
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+    title_picture_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Article
-        fields = ['id', 'subjects', 'created', 'title', 'title_picture', 'text']
+        fields = ['id', 'subjects', 'created', 'title', 'title_picture_url', 'text']
+
+    def get_title_picture_url(self, article):
+        request = self.context.get('request')
+        return request.scheme + "://" + request.META['HTTP_HOST'] + '/' + article.title_picture.url
 
 
 class EventSerializer(serializers.ModelSerializer):
--- Backend/DjangoBackend/Content/views.py
+++ Backend/DjangoBackend/Content/views.py
@@ -13,10 +13,10 @@ class MyOwnViewSet(ModelViewSet):
 
     def get_n(self, request, ordering='l', start="0", end="30"):
         if (ordering == 'l'):
-            content = self.queryset.order_by("-created")[int(start):int(end)]
+            objects = self.queryset.order_by("-created")[int(start):int(end)]
         if ordering == 'p':
             raise Http404
-        serializer = self.serializer_class(content, many=True)
+        serializer = self.serializer_class(objects, many=True)
         return Response(serializer.data)
 
 
