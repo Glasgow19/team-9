29cba54023f2a85f219c48c11c318c0ce522f748
ALLAN <allan.steven.pm@gmail.com>
2019-10-27 01:09:29 +0000

    kuba in the loop

--- dressCodeApp/screens/HomeScreen.js
+++ dressCodeApp/screens/HomeScreen.js
@@ -13,22 +13,48 @@ import {
 import { MonoText } from '../components/StyledText';
 import { Avatar } from "react-native-elements";
 import { Button } from 'react-native-elements';
+import { Card } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import Menu from '../navigation/NavigationMenu'
 
 export default function HomeScreen() {
+  const users = [
+   {
+      name: 'brynn',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg'
+   },
+   {
+      name: 'sarah',
+      avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/brynn/138.jpg'
+   },
+  ]
   return (
     <View style={styles.container}>
       <Menu/>
+      <ScrollView>
+      {
+        users.map((l,i) => 
+          <Card
+            title='HELLO WORLD'
+            image={require('../assets/images/robot-dev.png')}>
+            <Text style={{marginBottom: 10}}>
+              The idea with React Native Elements is more about component structure than actual design.
+            </Text>
+            <Button
+              icon={<Icon name='code' color='#ffffff' />}
+              buttonStyle={{borderRadius: 0, marginLeft: 0, marginRight: 0, marginBottom: 0}}
+              title='VIEW NOW' />
+          </Card>  
+        )
+      }
+      </ScrollView>
       <ScrollView
         style={styles.container}
         contentContainerStyle={styles.contentContainer}>
         <View style={styles.welcomeContainer}>
           <Image
             source={
-              __DEV__
-                ? require('../assets/images/logos/dressCODE_Final.png')
-                : require('../assets/images/robot-prod.png')
+              require('../assets/images/robot-prod.png')
             }
             style={styles.welcomeImage}
           />
